feab0ae6e340fbcc6394660af247600a05d66501
LIZ <gabyboob7@gmail.com>
2024-04-08 20:00:18 -0600

    Primer commit

---
+++ ScriptInsersion.sql
@@ -0,0 +1,31 @@
+
+
+INSERT INTO Usuario(NickName, Contrasenia, TipoUsuarioId, Activo)
+    VALUES ('LIZ', '12345', 2, 1);
+
+INSERT INTO Usuario(NickName, contrasenia, TipoUsuarioId, Activo)
+     VALUES ('ABI', '15467', 2, 1);
+
+INSERT INTO Usuario(NickName, contrasenia, TipoUsuarioId, Activo)
+    VALUES ('Rigo','23456', 2 ,1);
+
+INSERT INTO Usuario(NickName, contrasenia, TipoUsuarioId, Activo)
+     VALUES ('Wendy','98765',2 ,1);
+
+INSERT INTO Usuario(NickName, Contrasenia, TipoUsuarioId, Activo)
+    VALUES ('Juan', '34567', 2, 1);
+
+INSERT INTO Usuario(NickName, Contrasenia, TipoUsuarioId, Activo)
+    VALUES ('Maria', '78901', 2, 1);
+
+INSERT INTO Usuario(NickName, Contrasenia, TipoUsuarioId, Activo)
+    VALUES ('Pedro', '87654', 2, 1);
+
+INSERT INTO Usuario(NickName, Contrasenia, TipoUsuarioId, Activo)
+VALUES ('Laura', '54321', 2, 1);
+
+INSERT INTO Usuario(NickName, Contrasenia, TipoUsuarioId, Activo)
+VALUES ('Carlos', '45678', 2, 1);
+
+INSERT INTO Usuario(NickName, Contrasenia, TipoUsuarioId, Activo)
+VALUES ('Ana', '10234', 2, 1);
